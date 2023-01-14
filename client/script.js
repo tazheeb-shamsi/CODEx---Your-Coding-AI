a57e7ad62c5fb3977fee797bbc7d34eed741ea42
tazheeb <thshamsi@hotmail.com>
2023-01-14 17:36:53 +0530

    final commit

--- client/script.js
+++ client/script.js
@@ -84,18 +84,15 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch(
-    "https://codex-your-coding-ai-f1dq.onrender.com/",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: data.get("prompt"),
-      }),
-    }
-  );
+  const response = await fetch("https://codex-server-8l9d.onrender.com/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
 
   clearInterval(loadInterval);
   messageDiv.innerHTML = " ";
