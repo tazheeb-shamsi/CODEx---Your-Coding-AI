5dc024d9ab2fcb44b152cd3a5d00cb82166a4e08
tazheeb <thshamsi@hotmail.com>
2023-01-14 17:59:17 +0530

    final commit

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://codex-server-yvcl.onrender.com/", {
+  const response = await fetch("https://codex-server-yvcl.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
