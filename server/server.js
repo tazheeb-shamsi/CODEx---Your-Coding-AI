65ce73e6587fde95aeda9d0849a243d5c7c64854
tazheeb <thshamsi@hotmail.com>
2023-01-14 17:07:32 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
